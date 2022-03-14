c9646b07cf67d2230d3d04e87412bd4208e529a5
agui <55447192+GuiWonder@users.noreply.github.com>
2022-03-14 13:26:06 +0800

    Update EditTab.Designer.cs

--- MyLinks/MyLinks/EditTab.Designer.cs
+++ MyLinks/MyLinks/EditTab.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace MyLinks
+namespace MyLinks
 {
     partial class EditTab
     {/// <summary>
@@ -30,24 +30,20 @@
             this.checkBoxBG = new System.Windows.Forms.CheckBox();
             this.textBoxImg = new System.Windows.Forms.TextBox();
             this.checkBoxTiled = new System.Windows.Forms.CheckBox();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabelBKImage = new System.Windows.Forms.LinkLabel();
             this.textBoxTitle = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.listView = new System.Windows.Forms.ListView();
-            this.buttonAdd = new System.Windows.Forms.Button();
-            this.buttonRemove = new System.Windows.Forms.Button();
-            this.buttonInsert = new System.Windows.Forms.Button();
-            this.buttonUp = new System.Windows.Forms.Button();
-            this.buttonDown = new System.Windows.Forms.Button();
-            this.buttonClear = new System.Windows.Forms.Button();
-            this.buttonSave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonList = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // checkBoxBG
             // 
             this.checkBoxBG.AutoSize = true;
-            this.checkBoxBG.Location = new System.Drawing.Point(12, 12);
+            this.checkBoxBG.Location = new System.Drawing.Point(11, 53);
             this.checkBoxBG.Name = "checkBoxBG";
             this.checkBoxBG.Size = new System.Drawing.Size(96, 16);
             this.checkBoxBG.TabIndex = 0;
@@ -57,37 +53,41 @@
             // 
             // textBoxImg
             // 
-            this.textBoxImg.Location = new System.Drawing.Point(37, 34);
+            this.textBoxImg.AllowDrop = true;
+            this.textBoxImg.Location = new System.Drawing.Point(36, 75);
             this.textBoxImg.Multiline = true;
             this.textBoxImg.Name = "textBoxImg";
             this.textBoxImg.Size = new System.Drawing.Size(342, 43);
             this.textBoxImg.TabIndex = 1;
+            this.textBoxImg.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBoxImg_DragDrop);
+            this.textBoxImg.DragEnter += new System.Windows.Forms.DragEventHandler(this.TextBoxImg_DragEnter);
+            this.textBoxImg.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxImg_KeyPress);
             // 
             // checkBoxTiled
             // 
             this.checkBoxTiled.AutoSize = true;
-            this.checkBoxTiled.Location = new System.Drawing.Point(37, 83);
+            this.checkBoxTiled.Location = new System.Drawing.Point(36, 125);
             this.checkBoxTiled.Name = "checkBoxTiled";
-            this.checkBoxTiled.Size = new System.Drawing.Size(48, 16);
+            this.checkBoxTiled.Size = new System.Drawing.Size(96, 16);
             this.checkBoxTiled.TabIndex = 2;
-            this.checkBoxTiled.Text = "平铺";
+            this.checkBoxTiled.Text = "背景图片平铺";
             this.checkBoxTiled.UseVisualStyleBackColor = true;
             this.checkBoxTiled.CheckedChanged += new System.EventHandler(this.CheckBoxTiled_CheckedChanged);
             // 
-            // linkLabel1
+            // linkLabelBKImage
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(385, 42);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(29, 12);
-            this.linkLabel1.TabIndex = 3;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "浏览";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel1_LinkClicked);
+            this.linkLabelBKImage.AutoSize = true;
+            this.linkLabelBKImage.Location = new System.Drawing.Point(384, 78);
+            this.linkLabelBKImage.Name = "linkLabelBKImage";
+            this.linkLabelBKImage.Size = new System.Drawing.Size(29, 12);
+            this.linkLabelBKImage.TabIndex = 3;
+            this.linkLabelBKImage.TabStop = true;
+            this.linkLabelBKImage.Text = "浏览";
+            this.linkLabelBKImage.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabelBKImage_LinkClicked);
             // 
             // textBoxTitle
             // 
-            this.textBoxTitle.Location = new System.Drawing.Point(78, 106);
+            this.textBoxTitle.Location = new System.Drawing.Point(78, 12);
             this.textBoxTitle.Name = "textBoxTitle";
             this.textBoxTitle.Size = new System.Drawing.Size(300, 21);
             this.textBoxTitle.TabIndex = 9;
@@ -96,7 +96,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 111);
+            this.label1.Location = new System.Drawing.Point(10, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 10;
@@ -105,124 +105,53 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 141);
+            this.label2.Location = new System.Drawing.Point(12, 218);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 12);
             this.label2.TabIndex = 11;
             this.label2.Text = "列表";
             // 
-            // listView
+            // label3
             // 
-            this.listView.FullRowSelect = true;
-            this.listView.HideSelection = false;
-            this.listView.Location = new System.Drawing.Point(14, 156);
-            this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(400, 333);
-            this.listView.TabIndex = 12;
-            this.listView.UseCompatibleStateImageBehavior = false;
-            this.listView.View = System.Windows.Forms.View.Details;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "文字颜色";
             // 
-            // buttonAdd
+            // buttonList
             // 
-            this.buttonAdd.AllowDrop = true;
-            this.buttonAdd.AutoSize = true;
-            this.buttonAdd.Location = new System.Drawing.Point(14, 495);
-            this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(49, 23);
-            this.buttonAdd.TabIndex = 13;
-            this.buttonAdd.Text = "添加";
-            this.buttonAdd.UseVisualStyleBackColor = true;
-            this.buttonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            this.buttonList.Location = new System.Drawing.Point(70, 213);
+            this.buttonList.Name = "buttonList";
+            this.buttonList.Size = new System.Drawing.Size(75, 23);
+            this.buttonList.TabIndex = 23;
+            this.buttonList.Text = "内容";
+            this.buttonList.UseVisualStyleBackColor = true;
+            this.buttonList.Click += new System.EventHandler(this.ButtonList_Click);
             // 
-            // buttonRemove
+            // pictureBox1
             // 
-            this.buttonRemove.AllowDrop = true;
-            this.buttonRemove.AutoSize = true;
-            this.buttonRemove.Location = new System.Drawing.Point(69, 495);
-            this.buttonRemove.Name = "buttonRemove";
-            this.buttonRemove.Size = new System.Drawing.Size(49, 23);
-            this.buttonRemove.TabIndex = 14;
-            this.buttonRemove.Text = "移除";
-            this.buttonRemove.UseVisualStyleBackColor = true;
-            this.buttonRemove.Click += new System.EventHandler(this.ButtonRemove_Click);
-            // 
-            // buttonInsert
-            // 
-            this.buttonInsert.AllowDrop = true;
-            this.buttonInsert.AutoSize = true;
-            this.buttonInsert.Location = new System.Drawing.Point(124, 495);
-            this.buttonInsert.Name = "buttonInsert";
-            this.buttonInsert.Size = new System.Drawing.Size(49, 23);
-            this.buttonInsert.TabIndex = 15;
-            this.buttonInsert.Text = "插入";
-            this.buttonInsert.UseVisualStyleBackColor = true;
-            this.buttonInsert.Click += new System.EventHandler(this.ButtonInsert_Click);
-            // 
-            // buttonUp
-            // 
-            this.buttonUp.AllowDrop = true;
-            this.buttonUp.AutoSize = true;
-            this.buttonUp.Location = new System.Drawing.Point(179, 495);
-            this.buttonUp.Name = "buttonUp";
-            this.buttonUp.Size = new System.Drawing.Size(49, 23);
-            this.buttonUp.TabIndex = 16;
-            this.buttonUp.Text = "上移";
-            this.buttonUp.UseVisualStyleBackColor = true;
-            this.buttonUp.Click += new System.EventHandler(this.ButtonUp_Click);
-            // 
-            // buttonDown
-            // 
-            this.buttonDown.AllowDrop = true;
-            this.buttonDown.AutoSize = true;
-            this.buttonDown.Location = new System.Drawing.Point(234, 495);
-            this.buttonDown.Name = "buttonDown";
-            this.buttonDown.Size = new System.Drawing.Size(49, 23);
-            this.buttonDown.TabIndex = 17;
-            this.buttonDown.Text = "下移";
-            this.buttonDown.UseVisualStyleBackColor = true;
-            this.buttonDown.Click += new System.EventHandler(this.ButtonDown_Click);
-            // 
-            // buttonClear
-            // 
-            this.buttonClear.AllowDrop = true;
-            this.buttonClear.AutoSize = true;
-            this.buttonClear.Location = new System.Drawing.Point(289, 495);
-            this.buttonClear.Name = "buttonClear";
-            this.buttonClear.Size = new System.Drawing.Size(49, 23);
-            this.buttonClear.TabIndex = 18;
-            this.buttonClear.Text = "淸空";
-            this.buttonClear.UseVisualStyleBackColor = true;
-            this.buttonClear.Click += new System.EventHandler(this.ButtonClear_Click);
-            // 
-            // buttonSave
-            // 
-            this.buttonSave.AllowDrop = true;
-            this.buttonSave.AutoSize = true;
-            this.buttonSave.Location = new System.Drawing.Point(365, 495);
-            this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(49, 23);
-            this.buttonSave.TabIndex = 19;
-            this.buttonSave.Text = "保存";
-            this.buttonSave.UseVisualStyleBackColor = true;
-            this.buttonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(70, 161);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
             // 
             // EditTab
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 520);
-            this.Controls.Add(this.buttonSave);
-            this.Controls.Add(this.buttonClear);
-            this.Controls.Add(this.buttonDown);
-            this.Controls.Add(this.buttonUp);
-            this.Controls.Add(this.buttonInsert);
-            this.Controls.Add(this.buttonRemove);
-            this.Controls.Add(this.buttonAdd);
-            this.Controls.Add(this.listView);
+            this.ClientSize = new System.Drawing.Size(427, 286);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonList);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxTitle);
-            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.linkLabelBKImage);
             this.Controls.Add(this.checkBoxTiled);
             this.Controls.Add(this.textBoxImg);
             this.Controls.Add(this.checkBoxBG);
@@ -234,6 +163,7 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "修改栏";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -244,17 +174,12 @@
         private System.Windows.Forms.CheckBox checkBoxBG;
         private System.Windows.Forms.TextBox textBoxImg;
         private System.Windows.Forms.CheckBox checkBoxTiled;
-        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabelBKImage;
         private System.Windows.Forms.TextBox textBoxTitle;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListView listView;
-        private System.Windows.Forms.Button buttonAdd;
-        private System.Windows.Forms.Button buttonRemove;
-        private System.Windows.Forms.Button buttonInsert;
-        private System.Windows.Forms.Button buttonUp;
-        private System.Windows.Forms.Button buttonDown;
-        private System.Windows.Forms.Button buttonClear;
-        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonList;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
