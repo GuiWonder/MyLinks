c74466da096e9e91652ca1afe535c121bb409bdd
agui <55447192+GuiWonder@users.noreply.github.com>
2022-03-14 13:24:29 +0800

    Add files via upload

---
+++ MyLinks/MyLinks/EditLists.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace MyLinks
+{
+    partial class EditLists
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.AllowDrop = true;
+            this.buttonSave.AutoSize = true;
+            this.buttonSave.Location = new System.Drawing.Point(496, 3);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(49, 22);
+            this.buttonSave.TabIndex = 27;
+            this.buttonSave.Text = "保存";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.AllowDrop = true;
+            this.buttonClear.AutoSize = true;
+            this.buttonClear.Location = new System.Drawing.Point(278, 3);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(49, 22);
+            this.buttonClear.TabIndex = 26;
+            this.buttonClear.Text = "淸空";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.AllowDrop = true;
+            this.buttonDown.AutoSize = true;
+            this.buttonDown.Location = new System.Drawing.Point(223, 3);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(49, 22);
+            this.buttonDown.TabIndex = 25;
+            this.buttonDown.Text = "下移";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.AllowDrop = true;
+            this.buttonUp.AutoSize = true;
+            this.buttonUp.Location = new System.Drawing.Point(168, 3);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(49, 22);
+            this.buttonUp.TabIndex = 24;
+            this.buttonUp.Text = "上移";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.AllowDrop = true;
+            this.buttonInsert.AutoSize = true;
+            this.buttonInsert.Location = new System.Drawing.Point(113, 3);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(49, 22);
+            this.buttonInsert.TabIndex = 23;
+            this.buttonInsert.Text = "插入";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.AllowDrop = true;
+            this.buttonRemove.AutoSize = true;
+            this.buttonRemove.Location = new System.Drawing.Point(58, 3);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(49, 22);
+            this.buttonRemove.TabIndex = 22;
+            this.buttonRemove.Text = "移除";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.AllowDrop = true;
+            this.buttonAdd.AutoSize = true;
+            this.buttonAdd.Location = new System.Drawing.Point(3, 3);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(49, 22);
+            this.buttonAdd.TabIndex = 21;
+            this.buttonAdd.Text = "添加";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // listView
+            // 
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.FullRowSelect = true;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(3, 3);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(548, 460);
+            this.listView.TabIndex = 20;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.listView, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(554, 501);
+            this.tableLayoutPanel1.TabIndex = 28;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 8;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel2.Controls.Add(this.buttonAdd, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonSave, 7, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonRemove, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonClear, 5, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonInsert, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonDown, 4, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonUp, 3, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 469);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(548, 29);
+            this.tableLayoutPanel2.TabIndex = 21;
+            // 
+            // EditLists
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 501);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "EditLists";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "编辑列";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    }
+}
---
+++ MyLinks/MyLinks/EditLists.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyLinks
+{
+    public partial class EditLists : Form
+    {
+        FormMain f1;
+        int i;
+        public EditLists(FormMain form, int i1)
+        {
+            f1 = form;
+            i = i1;
+            InitializeComponent();
+            listView.Columns.Add("名称", 100, HorizontalAlignment.Left);
+            listView.Columns.Add("路径", 250, HorizontalAlignment.Left);
+            listView.Columns.Add("参数", 60, HorizontalAlignment.Left);
+            listView.Columns.Add("管理员运行", 100, HorizontalAlignment.Left);
+
+            foreach (FileInfoWithIcon file in f1.fileLists[i].list)
+            {
+                ListViewItem item = new ListViewItem
+                {
+                    Text = file.Name
+                };
+                item.SubItems.Add(file.fileInfo.FullName);
+                item.SubItems.Add(file.Arg);
+                item.SubItems.Add(file.RunAsA.ToString());
+                listView.Items.Add(item);
+            }
+            buttonAdd.Click += ButtonAdd_Click;
+            buttonRemove.Click += ButtonRemove_Click;
+            buttonInsert.Click += ButtonInsert_Click;
+            buttonUp.Click += ButtonUp_Click; 
+            buttonDown.Click += ButtonDown_Click;
+            buttonClear.Click += ButtonClear_Click;
+            buttonSave.Click += ButtonSave_Click;
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string file1 in openFileDialog.FileNames)
+                    {
+                        ListViewItem item = new ListViewItem
+                        {
+                            Text = System.IO.Path.GetFileName(file1)
+                        };
+                        item.SubItems.Add(file1);
+                        item.SubItems.Add("");
+                        item.SubItems.Add("False");
+                        listView.Items.Add(item);
+                    }
+                    listView.Items[listView.Items.Count - 1].Selected = true;
+                }
+            }
+        }
+
+        private void ButtonRemove_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                int i = listView.SelectedItems[0].Index;
+                foreach (ListViewItem item in listView.SelectedItems)
+                {
+                    listView.Items.Remove(item);
+                }
+                if (i < listView.Items.Count)
+                {
+                    listView.Items[i].Selected = true;
+                }
+                else if (listView.Items.Count > 0)
+                {
+                    listView.Items[i - 1].Selected = true;
+                }
+            }
+
+        }
+
+        private void ButtonInsert_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK && listView.SelectedItems.Count > 0)
+                {
+                    foreach (string file1 in openFileDialog.FileNames)
+                    {
+                        ListViewItem item = new ListViewItem
+                        {
+                            Text = System.IO.Path.GetFileName(file1)
+                        };
+                        item.SubItems.Add(file1);
+                        item.SubItems.Add("");
+                        item.SubItems.Add("False");
+                        listView.Items.Insert(listView.SelectedItems[0].Index, item);
+                    }
+                }
+            }
+        }
+
+        private void ButtonUp_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                int i = listView.SelectedItems[0].Index;
+                if (i > 0)
+                {
+                    ListViewItem listViewItem = listView.Items[i - 1];
+                    listView.Items.RemoveAt(i - 1);
+                    listView.Items.Insert(i + listView.SelectedItems.Count - 1, listViewItem);
+                }
+            }
+        }
+
+        private void ButtonDown_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                int i = listView.SelectedItems[0].Index;
+                if (i + listView.SelectedItems.Count < listView.Items.Count)
+                {
+                    ListViewItem listViewItem = listView.Items[i + listView.SelectedItems.Count];
+                    listView.Items.RemoveAt(i + listView.SelectedItems.Count);
+                    listView.Items.Insert(i, listViewItem);
+                }
+            }
+        }
+
+        private void ButtonClear_Click(object sender, EventArgs e) => listView.Items.Clear();
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            f1.fileLists[i].RemoveAll();
+            foreach (ListViewItem item in listView.Items)
+            {
+                f1.fileLists[i].AddFile(item.SubItems[1].Text);
+                f1.fileLists[i].list[f1.fileLists[i].list.Count - 1].Name = item.Text;
+                f1.fileLists[i].list[f1.fileLists[i].list.Count - 1].Arg = item.SubItems[2].Text;
+                f1.fileLists[i].list[f1.fileLists[i].list.Count - 1].RunAsA = bool.Parse(item.SubItems[3].Text);
+            }
+        }
+    }
+}
