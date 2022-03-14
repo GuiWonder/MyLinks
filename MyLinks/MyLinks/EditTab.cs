8e21eaf088c743102979178c96c69f284d9adb9f
agui <55447192+GuiWonder@users.noreply.github.com>
2022-03-14 13:25:22 +0800

    Update EditTab.cs

--- MyLinks/MyLinks/EditTab.cs
+++ MyLinks/MyLinks/EditTab.cs
@@ -28,25 +28,12 @@ namespace MyLinks
             checkBoxTiled.Checked = f1.listViews[i].BackgroundImageTiled;
             textBoxImg.Enabled = checkBoxBG.Checked;
             checkBoxTiled.Enabled = checkBoxBG.Checked;
-            linkLabel1.Enabled = checkBoxBG.Checked;
+            linkLabelBKImage.Enabled = checkBoxBG.Checked;
             textBoxTitle.Text = f1.tabControl.TabPages[i].Text;
-            listView.Columns.Add("名称", 100, HorizontalAlignment.Left);
-            listView.Columns.Add("路径", 200, HorizontalAlignment.Left);
-            listView.Columns.Add("参数", 60, HorizontalAlignment.Left);
-            listView.Columns.Add("管理员运行", 60, HorizontalAlignment.Left);
-
-            foreach (FileInfoWithIcon file in f1.fileLists[i].list)
-            {
-                ListViewItem item = new ListViewItem
-                {
-                    Text = file.Name
-                };
-                item.SubItems.Add(file.fileInfo.FullName);
-                item.SubItems.Add(file.Arg);
-                item.SubItems.Add(file.RunAsA.ToString());
-                listView.Items.Add(item);
-            }
+            pictureBox1.BackColor = f1.listViews[i].ForeColor;
         }
+
+        private void TextBoxTitle_TextChanged(object sender, EventArgs e) => f1.tabControl.TabPages[i].Text = textBoxTitle.Text;
 
         private void CheckBoxBG_CheckedChanged(object sender, EventArgs e)
         {
@@ -54,20 +41,26 @@ namespace MyLinks
             {
                 if (System.IO.File.Exists(textBoxImg.Text))
                 {
-                    f1.listViews[i].BackgroundImage = Image.FromFile(textBoxImg.Text);
+                    try
+                    {
+                        f1.listViews[i].BackgroundImage = Image.FromFile(textBoxImg.Text);
+                    }
+                    catch (Exception)
+                    {
+                        //f1.listViews[i].BackgroundImage = null;
+                    }
                 }
             }
             else
             {
                 f1.listViews[i].BackgroundImage = null;
             }
-
             textBoxImg.Enabled = checkBoxBG.Checked;
             checkBoxTiled.Enabled = checkBoxBG.Checked;
-            linkLabel1.Enabled = checkBoxBG.Checked;
+            linkLabelBKImage.Enabled = checkBoxBG.Checked;
         }
 
-        private void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void LinkLabelBKImage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog
             {
@@ -77,123 +70,60 @@ namespace MyLinks
             {
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    f1.backimage[i] = openFileDialog.FileName;
-                    textBoxImg.Text = f1.backimage[i];
-                    Image image = Image.FromFile(f1.backimage[i]);
-                    f1.listViews[i].BackgroundImage = image;
+                    textBoxImg.Text = openFileDialog.FileName;
+                    SetBKImage(openFileDialog.FileName);
                 }
+            }
+        }
+
+        private void TextBoxImg_DragDrop(object sender, DragEventArgs e)
+        {
+            ((TextBox)sender).Text = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            SetBKImage(((TextBox)sender).Text);
+        }
+
+        private void TextBoxImg_DragEnter(object sender, DragEventArgs e) => e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.All : DragDropEffects.None;
+
+        private void TextBoxImg_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                SetBKImage(textBoxImg.Text);
+            }
+        }
+
+        private void SetBKImage(string imgfile)
+        {
+            try
+            {
+                f1.listViews[i].BackgroundImage = Image.FromFile(imgfile);
+                f1.backimage[i] = imgfile;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(this, "文件读取错误！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void CheckBoxTiled_CheckedChanged(object sender, EventArgs e) => f1.listViews[i].BackgroundImageTiled = checkBoxTiled.Checked;
 
-        private void TextBoxTitle_TextChanged(object sender, EventArgs e) => f1.tabControl.TabPages[i].Text = textBoxTitle.Text;
-
-        private void ButtonAdd_Click(object sender, EventArgs e)
+        private void PictureBox1_Click(object sender, EventArgs e)
         {
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            using (ColorDialog colorDialog = new ColorDialog())
             {
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                if (colorDialog.ShowDialog() == DialogResult.OK)
                 {
-                    foreach (string file1 in openFileDialog.FileNames)
-                    {
-                        ListViewItem item = new ListViewItem
-                        {
-                            Text = System.IO.Path.GetFileName(file1)
-                        };
-                        item.SubItems.Add(file1);
-                        item.SubItems.Add("");
-                        item.SubItems.Add("False");
-                        listView.Items.Add(item);
-                    }
-                    listView.Items[listView.Items.Count - 1].Selected = true;
+                    Text = colorDialog.Color.ToArgb().ToString();
+                    pictureBox1.BackColor = colorDialog.Color;
+                    f1.listViews[i].ForeColor = colorDialog.Color;
                 }
             }
         }
 
-        private void ButtonRemove_Click(object sender, EventArgs e)
+        private void ButtonList_Click(object sender, EventArgs e)
         {
-            if (listView.SelectedItems.Count > 0)
-            {
-                int i = listView.SelectedItems[0].Index;
-                foreach (ListViewItem item in listView.SelectedItems)
-                {
-                    listView.Items.Remove(item);
-                }
-                if (i < listView.Items.Count)
-                {
-                    listView.Items[i].Selected = true;
-                }
-                else if (listView.Items.Count > 0)
-                {
-                    listView.Items[i - 1].Selected = true;
-                }
-            }
-
-        }
-
-        private void ButtonInsert_Click(object sender, EventArgs e)
-        {
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
-            {
-                if (openFileDialog.ShowDialog() == DialogResult.OK && listView.SelectedItems.Count > 0)
-                {
-                    foreach (string file1 in openFileDialog.FileNames)
-                    {
-                        ListViewItem item = new ListViewItem
-                        {
-                            Text = System.IO.Path.GetFileName(file1)
-                        };
-                        item.SubItems.Add(file1);
-                        item.SubItems.Add("");
-                        item.SubItems.Add("False");
-                        listView.Items.Insert(listView.SelectedItems[0].Index, item);
-                    }
-                }
-            }
-        }
-
-        private void ButtonUp_Click(object sender, EventArgs e)
-        {
-            if (listView.SelectedItems.Count > 0)
-            {
-                int i = listView.SelectedItems[0].Index;
-                if (i > 0)
-                {
-                    ListViewItem listViewItem = listView.Items[i - 1];
-                    listView.Items.RemoveAt(i - 1);
-                    listView.Items.Insert(i + listView.SelectedItems.Count - 1, listViewItem);
-                }
-            }
-
-        }
-
-        private void ButtonDown_Click(object sender, EventArgs e)
-        {
-            if (listView.SelectedItems.Count > 0)
-            {
-                int i = listView.SelectedItems[0].Index;
-                if (i + listView.SelectedItems.Count < listView.Items.Count)
-                {
-                    ListViewItem listViewItem = listView.Items[i + listView.SelectedItems.Count];
-                    listView.Items.RemoveAt(i + listView.SelectedItems.Count);
-                    listView.Items.Insert(i, listViewItem);
-                }
-            }
-        }
-
-        private void ButtonClear_Click(object sender, EventArgs e) => listView.Items.Clear();
-
-        private void ButtonSave_Click(object sender, EventArgs e)
-        {
-            f1.fileLists[i].RemoveAll();
-            foreach (ListViewItem item in listView.Items)
-            {
-                f1.fileLists[i].AddFile(item.SubItems[1].Text);
-                f1.fileLists[i].list[f1.fileLists[i].list.Count - 1].Name = item.Text;
-                f1.fileLists[i].list[f1.fileLists[i].list.Count - 1].Arg = item.SubItems[2].Text;
-                f1.fileLists[i].list[f1.fileLists[i].list.Count - 1].RunAsA = bool.Parse(item.SubItems[3].Text);
-            }
+            EditLists editLists = new EditLists(f1, i);
+            editLists.ShowDialog(this);
         }
     }
 }
