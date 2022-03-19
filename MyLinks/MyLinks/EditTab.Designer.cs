89ed82b279664a353e49d7a3f6c8703a27db1529
agui <55447192+GuiWonder@users.noreply.github.com>
2022-03-19 12:16:39 +0800

    Update EditTab.Designer.cs

--- MyLinks/MyLinks/EditTab.Designer.cs
+++ MyLinks/MyLinks/EditTab.Designer.cs
@@ -33,17 +33,18 @@ namespace MyLinks
             this.linkLabelBKImage = new System.Windows.Forms.LinkLabel();
             this.textBoxTitle = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.buttonList = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBoxFore = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBoxBack = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBack)).BeginInit();
             this.SuspendLayout();
             // 
             // checkBoxBG
             // 
             this.checkBoxBG.AutoSize = true;
-            this.checkBoxBG.Location = new System.Drawing.Point(11, 53);
+            this.checkBoxBG.Location = new System.Drawing.Point(12, 138);
             this.checkBoxBG.Name = "checkBoxBG";
             this.checkBoxBG.Size = new System.Drawing.Size(96, 16);
             this.checkBoxBG.TabIndex = 0;
@@ -54,10 +55,10 @@ namespace MyLinks
             // textBoxImg
             // 
             this.textBoxImg.AllowDrop = true;
-            this.textBoxImg.Location = new System.Drawing.Point(36, 75);
+            this.textBoxImg.Location = new System.Drawing.Point(46, 160);
             this.textBoxImg.Multiline = true;
             this.textBoxImg.Name = "textBoxImg";
-            this.textBoxImg.Size = new System.Drawing.Size(342, 43);
+            this.textBoxImg.Size = new System.Drawing.Size(333, 43);
             this.textBoxImg.TabIndex = 1;
             this.textBoxImg.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBoxImg_DragDrop);
             this.textBoxImg.DragEnter += new System.Windows.Forms.DragEventHandler(this.TextBoxImg_DragEnter);
@@ -66,7 +67,7 @@ namespace MyLinks
             // checkBoxTiled
             // 
             this.checkBoxTiled.AutoSize = true;
-            this.checkBoxTiled.Location = new System.Drawing.Point(36, 125);
+            this.checkBoxTiled.Location = new System.Drawing.Point(37, 210);
             this.checkBoxTiled.Name = "checkBoxTiled";
             this.checkBoxTiled.Size = new System.Drawing.Size(96, 16);
             this.checkBoxTiled.TabIndex = 2;
@@ -77,7 +78,7 @@ namespace MyLinks
             // linkLabelBKImage
             // 
             this.linkLabelBKImage.AutoSize = true;
-            this.linkLabelBKImage.Location = new System.Drawing.Point(384, 78);
+            this.linkLabelBKImage.Location = new System.Drawing.Point(385, 160);
             this.linkLabelBKImage.Name = "linkLabelBKImage";
             this.linkLabelBKImage.Size = new System.Drawing.Size(29, 12);
             this.linkLabelBKImage.TabIndex = 3;
@@ -87,9 +88,9 @@ namespace MyLinks
             // 
             // textBoxTitle
             // 
-            this.textBoxTitle.Location = new System.Drawing.Point(78, 12);
+            this.textBoxTitle.Location = new System.Drawing.Point(45, 12);
             this.textBoxTitle.Name = "textBoxTitle";
-            this.textBoxTitle.Size = new System.Drawing.Size(300, 21);
+            this.textBoxTitle.Size = new System.Drawing.Size(333, 21);
             this.textBoxTitle.TabIndex = 9;
             this.textBoxTitle.TextChanged += new System.EventHandler(this.TextBoxTitle_TextChanged);
             // 
@@ -98,57 +99,57 @@ namespace MyLinks
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(10, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.Size = new System.Drawing.Size(29, 12);
             this.label1.TabIndex = 10;
-            this.label1.Text = "本栏标题";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 218);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 12);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "列表";
+            this.label1.Text = "标题";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 167);
+            this.label3.Location = new System.Drawing.Point(10, 100);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(53, 12);
             this.label3.TabIndex = 21;
             this.label3.Text = "文字颜色";
             // 
-            // buttonList
+            // pictureBoxFore
             // 
-            this.buttonList.Location = new System.Drawing.Point(70, 213);
-            this.buttonList.Name = "buttonList";
-            this.buttonList.Size = new System.Drawing.Size(75, 23);
-            this.buttonList.TabIndex = 23;
-            this.buttonList.Text = "内容";
-            this.buttonList.UseVisualStyleBackColor = true;
-            this.buttonList.Click += new System.EventHandler(this.ButtonList_Click);
+            this.pictureBoxFore.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxFore.Location = new System.Drawing.Point(71, 94);
+            this.pictureBoxFore.Name = "pictureBoxFore";
+            this.pictureBoxFore.Size = new System.Drawing.Size(25, 25);
+            this.pictureBoxFore.TabIndex = 24;
+            this.pictureBoxFore.TabStop = false;
+            this.pictureBoxFore.Click += new System.EventHandler(this.PictureBoxFore_Click);
             // 
-            // pictureBox1
+            // label4
             // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(70, 161);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
-            this.pictureBox1.TabIndex = 24;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "背景颜色";
+            // 
+            // pictureBoxBack
+            // 
+            this.pictureBoxBack.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxBack.Location = new System.Drawing.Point(71, 51);
+            this.pictureBoxBack.Name = "pictureBoxBack";
+            this.pictureBoxBack.Size = new System.Drawing.Size(25, 25);
+            this.pictureBoxBack.TabIndex = 24;
+            this.pictureBoxBack.TabStop = false;
+            this.pictureBoxBack.Click += new System.EventHandler(this.PictureBoxBack_Click);
             // 
             // EditTab
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 286);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.buttonList);
+            this.ClientSize = new System.Drawing.Size(427, 271);
+            this.Controls.Add(this.pictureBoxBack);
+            this.Controls.Add(this.pictureBoxFore);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxTitle);
             this.Controls.Add(this.linkLabelBKImage);
@@ -162,8 +163,9 @@ namespace MyLinks
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "修改栏";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Text = "类别设置";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBack)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,9 +179,9 @@ namespace MyLinks
         private System.Windows.Forms.LinkLabel linkLabelBKImage;
         private System.Windows.Forms.TextBox textBoxTitle;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button buttonList;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxFore;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBoxBack;
     }
 }
