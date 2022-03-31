c2243e5b820962ebab529461b4f341c4146dc6f6
agui <55447192+GuiWonder@users.noreply.github.com>
2022-03-31 18:12:09 +0800

    Update EditIco.cs

--- MyLinks/MyLinks/EditIco.cs
+++ MyLinks/MyLinks/EditIco.cs
@@ -1,14 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Launcher
+namespace MyLinks
 {
     public partial class EditIco : Form
     {
