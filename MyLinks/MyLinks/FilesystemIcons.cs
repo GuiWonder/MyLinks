3f05d1e2dbf028b9f0bb17c6e29b4eaa4f32823f
agui <55447192+GuiWonder@users.noreply.github.com>
2022-04-23 21:38:31 +0800

    Update FilesystemIcons.cs

--- MyLinks/MyLinks/FilesystemIcons.cs
+++ MyLinks/MyLinks/FilesystemIcons.cs
@@ -11,16 +11,8 @@ namespace MyLinks
         private const int SHGFI_LARGEICON = 0x0;
 
         #region PublicIcons
-        //public static Icon ICON_DIRECTORY_16x = ExtractIconFromFileX16(@"C:\Windows\system32\shell32.dll", 3); //Icon auch in Shell32.dll,3
-        //public static Icon ICON_DIRECTORY_32x = ExtractIconFromFileX32(@"C:\Windows\system32\shell32.dll", 3); //Icon auch in Shell32.dll,3
         public static Icon ICON_FILE_16x = ExtractIconFromFileX16(@"C:\Windows\system32\shell32.dll", 0);
         public static Icon ICON_FILE_32x = ExtractIconFromFileX32(@"C:\Windows\system32\shell32.dll", 0);
-        //public static Icon ICON_NETWORK_16x = ExtractIconFromFileX16(@"C:\Windows\system32\shell32.dll", 17);
-        //public static Icon ICON_NETWORK_32x = ExtractIconFromFileX32(@"C:\Windows\system32\shell32.dll", 17);
-        //public static Icon ICON_COMPUTER_16x = ExtractIconFromFileX16(@"C:\Windows\system32\shell32.dll", 15);
-        //public static Icon ICON_COMPUTER_32x = ExtractIconFromFileX32(@"C:\Windows\system32\shell32.dll", 15);
-        //public static Icon ICON_NETWORKFOLDER_16x = ExtractIconFromFileX16(@"C:\Windows\system32\shell32.dll", 275);
-        //public static Icon ICON_NETWORKFOLDER_32x = ExtractIconFromFileX32(@"C:\Windows\system32\shell32.dll", 275);
         #endregion
 
         public static Icon SmallIcon(string pfad)
@@ -61,11 +53,8 @@ namespace MyLinks
         public static extern int ExtractIconEx(string sFile, int iIndex, out IntPtr piLargeVersion, out IntPtr piSmallVersion, int amountIcons);
         public struct SHFILEINFO
         {
-            // : icon
             public IntPtr hIcon;
-            // : icondex
             public int iIcon;
-            // : SFGAO_ flags
             public int dwAttributes;
             [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
             public string szDisplayName;
@@ -102,15 +91,8 @@ namespace MyLinks
         #endregion
 
         #region PublicMethods
-        /// <summary>
-        /// Gibt das kleine Icon des Pfades aus
-        /// </summary>
-        /// <param name="pfad">Der Pfad zu einem Ordner oder Datei</param>
-        /// <returns></returns>
         public static Icon GetSmallIcon(string pfad)
         {
-            //IntPtr hImgSmall = default(IntPtr);
-            //The handle to the system image list.
             SHFILEINFO shinfo = new SHFILEINFO
             {
                 szDisplayName = new string((char)0, 260),
@@ -120,15 +102,8 @@ namespace MyLinks
             return Icon.FromHandle(shinfo.hIcon);
         }
 
-        /// <summary>
-        /// Gibt das große Icon des Pfades aus
-        /// </summary>
-        /// <param name="pfad">Der Pfad zu einem Ordner oder Datei</param>
-        /// <returns></returns>
         public static Icon GetLargeIcon(string pfad)
         {
-            //IntPtr hImgSmall = default(IntPtr);
-            //The handle to the system image list.
             SHFILEINFO shinfo = new SHFILEINFO
             {
                 szDisplayName = new string((char)0, 260),
@@ -138,103 +113,6 @@ namespace MyLinks
             return Icon.FromHandle(shinfo.hIcon);
         }
 
-        /*
-        /// <summary>
-        /// Gibt das Icon anhand der Extension aus
-        /// </summary>
-        /// <param name="extension">Die Erweiterung im Format '.extension'</param>
-        /// <returns></returns>
-        public static Icon GetIconByExtension_x16(string extension)
-        {
-            RegistryKey rk = Registry.ClassesRoot.OpenSubKey(extension);
-            //Extension-Key wird geöffnet
-            string assockey = (string)rk.GetValue("", "");
-            rk = Registry.ClassesRoot.OpenSubKey(assockey);
-            //Wechseln auf anderen Key
-            //Wert aus Default-Wert wird in das Textfeld geschrieben
-            Icon imgicon = null;
-            try
-            {
-                RegistryKey di = rk.OpenSubKey("DefaultIcon");
-                //Ruft den SubKey für das DefaultIcon auf
-                string defaulticon = (string)di.GetValue("", "");
-                //Wert wird in TextBox geschrieben
-                if (defaulticon.Length > 0)
-                {
-                    try
-                    {
-                        int komma = defaulticon.IndexOf(",");
-                        //Wenn kein Komma enthalten ist, wird -1 zurückgegeben
-                        if (komma != -1)
-                        {
-                            int ii = Convert.ToInt32(defaulticon.Substring(komma + 1).Trim());
-                            //Imageindex wird ermittelt
-                            imgicon = ExtractIconFromFileX16(defaulticon.Substring(0, komma), ii);
-                        }
-                        else
-                        {
-                            imgicon = ExtractIconFromFileX16(defaulticon, 0);
-                        }
-                    }
-                    catch
-                    {
-                    }
-                }
-            }
-            catch
-            {
-            }
-            return imgicon;
-        }
-
-        /// <summary>
-        /// Gibt das Icon anhand der Extension aus
-        /// </summary>
-        /// <param name="extension">Die Erweiterung im Format '.extension'</param>
-        /// <returns></returns>
-        public static Icon GetIconByExtension_x32(string extension)
-        {
-            RegistryKey rk = Registry.ClassesRoot.OpenSubKey(extension);
-            //Extension-Key wird geöffnet
-            string assockey = (string)rk.GetValue("", "");
-            rk = Registry.ClassesRoot.OpenSubKey(assockey);
-            //Wechseln auf anderen Key
-            //Wert aus Default-Wert wird in das Textfeld geschrieben
-            Icon imgicon = null;
-            try
-            {
-                RegistryKey di = rk.OpenSubKey("DefaultIcon");
-                //Ruft den SubKey für das DefaultIcon auf
-                string defaulticon = (string)di.GetValue("", "");
-                //Wert wird in TextBox geschrieben
-                if (defaulticon.Length > 0)
-                {
-                    try
-                    {
-                        int komma = defaulticon.IndexOf(",");
-                        //Wenn kein Komma enthalten ist, wird -1 zurückgegeben
-                        if (komma != -1)
-                        {
-                            int ii = Convert.ToInt32(defaulticon.Substring(komma + 1).Trim());
-                            //Imageindex wird ermittelt
-                            imgicon = ExtractIconFromFileX32(defaulticon.Substring(0, komma), ii);
-                        }
-                        else
-                        {
-                            imgicon = ExtractIconFromFileX32(defaulticon, 0);
-                        }
-                    }
-                    catch
-                    {
-                    }
-                }
-            }
-            catch
-            {
-            }
-            return imgicon;
-        }
-        */
         #endregion
     }
 }
